cb6b96b24031555ddd312008f7c84bc25b748638
squirmlabs <squirmlabs@gmail.com>
2019-08-11 10:21:45 -0700

    Configure babel overrides, add bootstrap, move App

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* .eslintrc.js */
+module.exports = {
+  extends: [
+    'react-app', // for editor
+    // 'eslint:recommended',
+    // 'plugin:prettier/recommended',
+    // 'prettier/react'
+  ],
+  parser: 'babel-eslint',
+  plugins: ['babel'],
+  rules: {
+    // quotes: ['error', 'single'],
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'babel/no-unused-expressions': 'error',
+    // 'no-unused-expressions': 'off',
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     singleQuote: true,
+    //     semi: true,
+    //     trailingComma: true,
+    //     jsxBracketSameLine: true
+    //   }
+    // ]
+  }
+};
---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import CalculatorController from '../CalculatorMVC/controller/CalculatorController';
+import CalculatorPresenter from '../CalculatorMVP/presenter/CalculatorPresenter';
+import CalculatorModel from '../CalculatorMVP/model/CalculatorModel';
+import Calculator from '../CalculatorMVP/view/Calculator.jsx';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    let calcController = new CalculatorController();
+    calcController.init();
+    const mainModel = new CalculatorModel();
+    const mainPresenter = new CalculatorPresenter();
+    mainPresenter.init(mainModel, Calculator);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
