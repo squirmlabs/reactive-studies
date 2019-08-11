365ecb929b49f750473d034435c8b50514324bd0
squirmlabs <squirmlabs@gmail.com>
2019-08-11 14:31:26 -0700

    Add Calculator actions

---
+++ src/actions/CalculatorActions.js
@@ -0,0 +1,23 @@
+export const CALCULATE = 'Calculate';
+export const INIT = 'InitCalculate';
+export const APPEND = 'AppendValue';
+
+export function calculate() {
+  return {
+    type: CALCULATE
+  };
+}
+
+export function reset() {
+  return {
+    type: INIT,
+    result: 0
+  };
+}
+
+export function appendValue(value) {
+  return {
+    type: APPEND,
+    toAppend: value
+  };
+}
