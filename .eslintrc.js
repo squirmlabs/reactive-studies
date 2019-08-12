c252a745fa5b0a654b6f69737b52732beaaa4ee3
squirmlabs <squirmlabs@gmail.com>
2019-08-11 21:44:12 -0700

    Add eslint hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 /* .eslintrc.js */
 module.exports = {
   extends: [
-    'react-app', // for editor
+    'react-app' // for editor
     // 'eslint:recommended',
     // 'plugin:prettier/recommended',
     // 'prettier/react'
   ],
+
   parser: 'babel-eslint',
-  plugins: ['babel'],
+  plugins: ['babel', 'react-hooks'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error'
+
     // quotes: ['error', 'single'],
     // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // 'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
