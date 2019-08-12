289d988175216b8fa3e1389ce238ebdb17246e7c
squirmlabs <squirmlabs@gmail.com>
2019-08-11 20:48:09 -0700

    Add redux to App

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { LiteEventEmitter } from 'lite-event-emitter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
 import logo from './logo.svg';
@@ -18,6 +20,10 @@ import CalcMVVM from '../components/calculator-mvvm/view/Calculator';
 import CalculatorViewModel from '../components/calculator-mvvm/viewmodel/CalculatorViewModel';
 import CalcModelMVVM from '../components/calculator-mvvm/model/CalculatorModel';
 
+// Redux
+import CalculatorContainer from '../containers/CalculatorContainer';
+import CalculatorReducer from '../reducers/CalculatorReducer';
+
 // Other Components
 import SimpleButton from '../components/button';
 
@@ -26,7 +32,7 @@ import './App.css';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    
+
     // MVC
     let calcController = new CalculatorController();
     calcController.init();
@@ -40,11 +46,22 @@ export default class App extends React.Component {
     const model = new CalcModelMVVM();
     const emitter = new LiteEventEmitter();
     const vm = new CalculatorViewModel(model, emitter); // eslint-disable-line no-unused-vars
-
     const cont = document.getElementById('calcMVVM');
+
     ReactDOM.render(
       <CalcMVVM emitter={emitter} initValue={model.total} />,
       cont
+    );
+
+    // Redux
+    const store = createStore(CalculatorReducer);
+    const calcContainer = document.getElementById('calcContainer');
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <CalculatorContainer />
+      </Provider>,
+      calcContainer
     );
   }
   render() {
