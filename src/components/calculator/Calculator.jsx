892e684b9a4f4e73f78b866b14ebd634a3f4bd95
squirmlabs <squirmlabs@gmail.com>
2019-08-11 19:30:38 -0700

    Document format

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,39 +1,42 @@
-import React from "react";
-import {ulStyle, acStyle, btnStyle, displayStyle} from "./Calculator.css";
+import React from 'react';
+import { ulStyle, acStyle, btnStyle, displayStyle } from './Calculator.css';
 
-export default class Calculator extends React.Component{
-    constructor(props){
-        super(props);
-        this.onBtnClicked = this.props.onButtonClicked;
-    }
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onBtnClicked = this.props.onButtonClicked;
+  }
 
-    createRow(id, ...labels){
-        let items = labels.map((value, index) => {
-            return <li key={`${id}_${index}`}
-                       style={value === "AC"? acStyle : btnStyle}
-                       onClick={::this.onBtnClicked}>
-                       {value}
-                   </li>;
-        })
+  createRow(id, ...labels) {
+    let items = labels.map((value, index) => {
+      return (
+        <li
+          key={`${id}_${index}`}
+          style={value === 'AC' ? acStyle : btnStyle}
+          onClick={::this.onBtnClicked}
+        >
+          {value}
+        </li>
+      );
+    });
 
-        return(
-            <ul key={id} style={ulStyle}>
-                {items}
-            </ul>
-        )
-        
-    }
+    return (
+      <ul key={id} style={ulStyle}>
+        {items}
+      </ul>
+    );
+  }
 
-    render(){
-        return(
-            <div>
-                <div style={displayStyle}>{this.props.result}</div>
-                {this.createRow("row1", "AC")}
-                {this.createRow("row2", "7", "8", "9", "/")}
-                {this.createRow("row3", "4", "5", "6", "*")}
-                {this.createRow("row4", "1", "2", "3", "-")}
-                {this.createRow("row5", "0", ".", "=", "+")}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div style={displayStyle}>{this.props.result}</div>
+        {this.createRow('row1', 'AC')}
+        {this.createRow('row2', '7', '8', '9', '/')}
+        {this.createRow('row3', '4', '5', '6', '*')}
+        {this.createRow('row4', '1', '2', '3', '-')}
+        {this.createRow('row5', '0', '.', '=', '+')}
+      </div>
+    );
+  }
 }
